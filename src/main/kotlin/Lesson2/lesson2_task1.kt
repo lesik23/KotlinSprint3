ef440b746c8a8871163ed27d1c6c0cee2cc66692
Lesik <Lesik_kz23@mail.ru>
2025-05-10 07:48:26 +0500

    KS-1-5 do lesson1_task5.kt

---
+++ src/main/kotlin/Lesson2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson2
+
+fun main() {
+
+    val grade1 = 3
+    val grade2 = 4
+    val grade3 = 3
+    val grade4 = 5
+
+    val averageGrade = (grade1 + grade2 + grade3 + grade4) / 4f
+    println(averageGrade)
+
+}
