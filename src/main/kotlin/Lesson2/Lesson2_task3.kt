ca3e70cbaf4c05f2826655778dc1dd89d449cff6
Lesik_kz23 <lesik_kz23@mail.kz>
2025-05-16 20:23:39 +0300

    KS-2-3_do_lesson2_task3

---
+++ src/main/kotlin/Lesson2/Lesson2_task3.kt
@@ -0,0 +1,18 @@
+package org.example.Lesson2
+
+fun main() {
+
+    val departureHours = 9
+    val departureMinutes = 39
+    val minutesOfWay = 457
+    val minutesPerHour = 60
+
+    val totalHours = (departureMinutes + minutesOfWay) / minutesPerHour
+
+    val arrivalHour = totalHours + departureHours
+    print(arrivalHour)
+    print(":")
+    val arrivalMinutes = (departureMinutes + minutesOfWay) - totalHours * minutesPerHour
+    print(arrivalMinutes)
+
+}
