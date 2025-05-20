73fa80edb2117b4ed87a5f097429cfc22e8d6100
Lesik <Lesik_kz23@mail.ru>
2025-05-20 21:45:09 +0500

    KS-3-1_do_lesson3_task1

---
+++ src/main/kotlin/Lesson3/Lesson3_task1.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson3
+
+fun main() {
+
+    val day = "Добрый день,"
+    val evening = "Добрый вечер,"
+    val userName = "Олеся!"
+    var greeting = "$day $userName"
+    println(greeting)
+    greeting = "$evening $userName"
+    println(greeting)
+
+}
