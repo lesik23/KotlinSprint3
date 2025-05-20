345ba3336a47853406db6d4b9940e3e9536acd15
Lesik <Lesik_kz23@mail.ru>
2025-05-20 22:04:58 +0500

    KS-3-2_do_lesson3_task2

---
+++ src/main/kotlin/Lesson3/Lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson3
+
+fun main() {
+
+    var lastName = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var age = 20
+    println("$lastName $name $patronymic $age лет")
+
+    lastName = "Сидорова"
+    age = 22
+    println("$lastName $name $patronymic $age года")
+
+}
