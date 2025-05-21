2da3c2c850471ee9e54a2ae4c0c5bd8028cc9bb6
Lesik <Lesik_kz23@mail.ru>
2025-05-21 21:45:08 +0500

    KS-3-3_do_lesson3_task3

---
+++ src/main/kotlin/Lesson3/Lesson3_task3.kt
@@ -0,0 +1,12 @@
+package org.example.Lesson3
+
+fun main() {
+
+    var number = 4
+    println("$number x 1 = ${number * 1} \n$number x 2 = ${number * 2} " +
+            "\n$number x 3 = ${number * 3} \n$number x 4 = ${number * 4}" +
+        "\n$number x 5 = ${number * 5} \n$number x 6 = ${number * 6}" +
+    "\n$number x 7 = ${number * 7} \n$number x 8 = ${number * 8}" +
+            "\n$number x 9 = ${number * 9} \n$number x 10 = ${number * 10}")
+
+}
