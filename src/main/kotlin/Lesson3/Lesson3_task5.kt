2c758b8a949de7ac62f1ce280dbd13ff81ccfdd6
Lesik <Lesik_kz23@mail.ru>
2025-05-22 18:53:38 +0500

    KS-3-5_do_lesson3_task5_ver.2

--- src/main/kotlin/Lesson3/Lesson3_task5.kt
+++ src/main/kotlin/Lesson3/Lesson3_task5.kt
@@ -1,14 +1,15 @@
 package org.example.Lesson3
 
+
 fun main() {
 
     val string = "D2-D4;0"
-    val result = string.split("-")
+    val result = string.split("-",";")
     var whereFrom = (result[0])
     println(whereFrom)
-    var partTwoOfResult = result[1].split(";")
-    println(partTwoOfResult[0])
-    var strokeNumber = partTwoOfResult[1]
+    var where = (result[1])
+    println(where)
+    var strokeNumber = (result[2])
     println(strokeNumber)
 
 }
