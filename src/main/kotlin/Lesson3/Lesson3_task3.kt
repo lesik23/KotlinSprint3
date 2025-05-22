b15c7234b2f91f1457f44a12069bb00fbee21746
Lesik <Lesik_kz23@mail.ru>
2025-05-22 19:08:51 +0500

    KS-3-3_do_lesson3_task3_ver.2

--- src/main/kotlin/Lesson3/Lesson3_task3.kt
+++ src/main/kotlin/Lesson3/Lesson3_task3.kt
@@ -3,10 +3,17 @@ package org.example.Lesson3
 fun main() {
 
     var number = 4
-    println("$number x 1 = ${number * 1} \n$number x 2 = ${number * 2} " +
-            "\n$number x 3 = ${number * 3} \n$number x 4 = ${number * 4}" +
-        "\n$number x 5 = ${number * 5} \n$number x 6 = ${number * 6}" +
-    "\n$number x 7 = ${number * 7} \n$number x 8 = ${number * 8}" +
-            "\n$number x 9 = ${number * 9} \n$number x 10 = ${number * 10}")
+    var answer= """
+        $number x 1 = ${number * 1} 
+        $number x 2 = ${number * 2} 
+        $number x 3 = ${number * 3}
+        $number x 4 = ${number * 4}
+        $number x 5 = ${number * 5}
+        $number x 6 = ${number * 6}
+        $number x 7 = ${number * 7}
+        $number x 8 = ${number * 8}
+        $number x 9 = ${number * 9}
+        $number x 10 = ${number * 10}""".trimIndent()
+    println(answer)
 
 }
