319ac21c5873d2d9f9f81d656b66f6a1f388521a
Lesik <Lesik_kz23@mail.ru>
2025-05-24 06:58:49 +0500

    KS-4-4_do_lesson4_task4_ver.2

--- src/main/kotlin/Lesson4/lesson4_task4.kt
+++ src/main/kotlin/Lesson4/lesson4_task4.kt
@@ -3,15 +3,11 @@ package org.example.Lesson4
 fun main() {
 
     var trainingDay = 5
-    var hands: Boolean = trainingDay % 2 != 0
-    var legs: Boolean = trainingDay % 2 == 0
-    var back: Boolean = trainingDay % 2 == 0
-    var press: Boolean = trainingDay % 2 != 0
     println("""
-        Упражнения для рук: $hands
-        Упражнения для ног: $legs
-        Упражнения для спины: $back
-        Упражнения для пресса: $press     
+        Упражнения для рук: ${trainingDay % 2 != 0}
+        Упражнения для ног: ${trainingDay % 2 == 0}
+        Упражнения для спины: ${trainingDay % 2 == 0}
+        Упражнения для пресса: ${trainingDay % 2 != 0}   
     """.trimIndent())
 
 }
