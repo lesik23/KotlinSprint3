1defc5e78bffa048f1820c70a5f2fc9ec096a6c9
Lesik <Lesik_kz23@mail.ru>
2025-05-24 07:13:28 +0500

    KS-4-3_do_lesson4_task3_ver.2

--- src/main/kotlin/Lesson4/lesson4_task3.kt
+++ src/main/kotlin/Lesson4/lesson4_task3.kt
@@ -2,11 +2,12 @@ package org.example.Lesson4
 
 fun main() {
 
-    var weatherIsSunnyToday:Boolean = true
-    var tentIsOpen: Boolean = true
-    var airHumidity: Int = 20
-    var season: String = "зима"
-    var comparisonResult: Boolean = (weatherIsSunnyToday == true) && (tentIsOpen == true) && (airHumidity == 20) && (season != "зима")
+    var comparisonResult: Boolean = (WEATHER_IS_SUNNY_TODAY == true) && (TENT_IS_OPEN == true) && (AIR_HUMIDITY == 20) && (SEASON != "зима")
     println("Благоприятные ли условия сейчас для роста бобовых? $comparisonResult" )
 
 }
+
+const val WEATHER_IS_SUNNY_TODAY: Boolean = true
+const val TENT_IS_OPEN: Boolean = true
+const val AIR_HUMIDITY: Int =20
+const val SEASON: String = "зима"
