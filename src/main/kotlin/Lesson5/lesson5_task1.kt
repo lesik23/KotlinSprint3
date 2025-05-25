2eb8c755f32659413cfa19c195b99e78c2b36d4c
Lesik <Lesik_kz23@mail.ru>
2025-05-25 16:31:32 +0500

    KS-4-5_do_lesson4_task5_ver2

---
+++ src/main/kotlin/Lesson5/lesson5_task1.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson5
+
+fun main() {
+
+    print("Пожалуйста, докажите, что вы не бот. Решите несложный пример: 7+3=")
+    var answer = readln().toInt()
+    if (answer == RIGHT_ANSWER) {
+        println("Добро пожаловать!")
+    }
+    else {
+        println("Вход запрещён")
+    }
+
+}
+
+const val RIGHT_ANSWER = 10
