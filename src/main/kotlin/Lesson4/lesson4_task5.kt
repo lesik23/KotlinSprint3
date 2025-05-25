2ab3a175a59b82999f324318807bc36d5d9a024e
Lesik <Lesik_kz23@mail.ru>
2025-05-25 09:05:53 +0500

    KS-4-5_do_lesson4_task5_ver2

--- src/main/kotlin/Lesson4/lesson4_task5.kt
+++ src/main/kotlin/Lesson4/lesson4_task5.kt
@@ -2,17 +2,21 @@ package org.example.Lesson4
 
 fun main() {
 
+    val minimumCrew = 50
+    val optimumCrew = 70
+    val minimumProvision = 50
+
     print("Есть ли повреждения?: ")
-    val damage: Boolean = readln() == "да"
+    val damage = readln()
     print("Количество экипажа: ")
     var numberOfCrew = readln().toInt()
     print("Количество ящиков с провизией: ")
     var numberOfBoxesWithProvision = readln().toInt()
     print("Благоприятная ли погода?: ")
-    val goodWeather: Boolean = readln() == "да"
-    println("Корабль может отправиться в плавание: ${(damage != true) && (numberOfCrew in 55..70) &&
-            (numberOfBoxesWithProvision > 50) && (goodWeather == true) || (damage == true) && (numberOfCrew == 70) && 
-            (numberOfBoxesWithProvision >= 50) &&(goodWeather == true)}")
+    val goodWeather = readln()
+    println("Корабль может отправиться в плавание: ${(damage != "да") && (numberOfCrew in minimumCrew..optimumCrew) &&
+            (numberOfBoxesWithProvision > minimumProvision) && (goodWeather == "да") || (damage == "да") && (numberOfCrew == optimumCrew) && 
+            (numberOfBoxesWithProvision >= minimumProvision) &&(goodWeather == "да")}")
 
 
 }
