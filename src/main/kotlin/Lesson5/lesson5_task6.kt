8ca49031e3799163edc13e40585f0cbe2d85ae7d
Lesik <Lesik_kz23@mail.ru>
2025-05-27 10:39:38 +0500

    KS-5-5 do_lesson5_task5

---
+++ src/main/kotlin/Lesson5/lesson5_task6.kt
@@ -0,0 +1,22 @@
+package org.example.Lesson5
+
+fun main() {
+
+    print("Введите ваш вес в килограммах: ")
+    val weight = readln().toFloat()
+    print("Ведите ваш рост в сантиметрах: ")
+    val height = readln().toFloat()
+    val heightInMeters = height / 100
+    val heightSquared = heightInMeters * heightInMeters
+    val bodyMassIndex = weight / heightSquared
+    val categoryBMI = when {
+        bodyMassIndex < 18.5 -> "недостаточная масса тела."
+        bodyMassIndex >= 18.5 && bodyMassIndex < 25 -> "нормальная масса тела."
+        bodyMassIndex >= 25 && bodyMassIndex <= 30 -> "избыточная масса тела."
+        bodyMassIndex >= 30 -> "Ожирение"
+        else -> "Ошибка"
+    }
+    val formatedBMI = String.format("%.2f", bodyMassIndex)
+    println("Ваш индекс массы тела равен $formatedBMI. Это означает,что у вас $categoryBMI")
+
+}
